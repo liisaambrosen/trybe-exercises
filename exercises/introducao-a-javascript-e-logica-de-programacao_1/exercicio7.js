e128c0d8ae390c9d01573e63446cec828f167634
liisaambrosen <liisa.ambrosen@gmail.com>
2020-10-23 18:29:52 -0300

    adiciona exercicio 7

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
@@ -0,0 +1,17 @@
+let porcentagem = -2;
+
+if (porcentagem >= 90) {
+    console.log("A");
+} else if (porcentagem >= 80) {
+    console.log("B");
+} else if (porcentagem >= 70) {
+    console.log("C");
+} else if (porcentagem >= 60) {
+    console.log("D");
+} else if (porcentagem >= 50) {
+    console.log("E");
+} else if (porcentagem < 50 && porcentagem > 0) {
+    console.log("F");
+} else {
+    console.log("porcentagem inválida");
+}
