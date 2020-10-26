f4c4254ddad8749066a5c11b3c09dcfbd51946f8
liisaambrosen <liisa.ambrosen@gmail.com>
2020-10-26 19:05:18 -0300

    adiciona exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
@@ -7,4 +7,9 @@ for (index = 0; index < numbers.length; index += 1) {
     media = soma / numbers.length;
 }
 
-console.log(media); 
+console.log(media);
+if (media > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor que 20");
+}
