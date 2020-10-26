42aaf2515674890ae6ea75b210142985c6209bf8
liisaambrosen <liisa.ambrosen@gmail.com>
2020-10-26 18:56:27 -0300

    adiciona exercicio 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio2.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (indice = 0; indice < numbers.length; indice += 1) {
+    soma = soma + numbers[indice];
+}
+
+console.log(soma);
