c912a987c7963778d1c9c6eea2151f2eb8a35d2a
liisaambrosen <liisa.ambrosen@gmail.com>
2020-10-28 19:33:21 -0300

    resolvendo exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio1-1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio1-1.js
@@ -2,14 +2,31 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
   };
 
   // console.log('Bem vinda, ' + info.personagem);
 
-info.recorrente = "Sim";
+let infoDois = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain",
+    nota: "O último McPatinhas",
+    recorrente: "Sim"
+}
 
 // console.log(info);
 
-for (let key in info) {
-    console.log(key);
+//for (let key in info) {
+//    console.log(key);
+//}
+
+for (let properties in info) {
+if (info[properties] === info.recorrente &&
+    info[properties] === "Sim" &&
+    infoDois[properties] === "Sim") {
+    console.log("Ambos recorrentes");
+} else {
+    console.log(info[properties] + " e " + infoDois[properties]);
 }
+}
+
